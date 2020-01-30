dd0b71deab3f3874492e4999bd85095960fbd502
Pierre Cayol <pierre.cayol@gmail.com>
2020-01-30 08:49:22 +0100

    :lipstick: Style slider desktop

--- components/MovieCard/MovieCard.js
+++ components/MovieCard/MovieCard.js
@@ -1,14 +1,17 @@
 import './MovieCard.scss';
 import React from 'react';
+import { head } from 'lodash/fp';
 
 const baseUrl = 'http://image.tmdb.org/t/p/w185/';
 
 const MovieCard = ({ poster_path, original_title, release_date }) => {
+  const date = release_date.match(/\d{4}/);
+  console.log(date);
   return (
     <div className="movie-card">
       <img src={`${baseUrl}${poster_path}`} alt={original_title} />
       <p className="movie-card__title">{original_title}</p>
-      <p className="movie-card__date">{release_date}</p>
+      <p className="movie-card__date">{head(date)}</p>
     </div>
   );
 };
